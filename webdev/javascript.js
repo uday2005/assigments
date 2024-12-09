0812945085c47126e4f39de14e4a9f2268b27791
Uday <24b2729@iitb.ac.in>
2024-12-10 00:02:50 +0530

    some changes again in background

--- webdev/javascript.js
+++ webdev/javascript.js
@@ -1,6 +1,3 @@
-let age = 11;
-console.log(age); // outputs 11 to the console
-
-age = 54;
- 
-console.log(age); // what will be output now?
+let h2 = document.querySelector("you");
+console.dir(h2);
+console.log(h2)
